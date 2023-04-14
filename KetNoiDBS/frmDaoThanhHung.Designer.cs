2c39729b398f90dfbb4d220cd29d6d587d869208
Thanh <hungktpm1406@gmail.com>
2023-04-14 22:36:13 +0700

    Add project files.

---
+++ KetNoiDBS/frmDaoThanhHung.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace KetNoiDBS
+{
+    partial class frmDaoThanhHung
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.datagrdviewKhoa = new System.Windows.Forms.DataGridView();
+            this.txtMaLop = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbKhoa = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTenLop = new System.Windows.Forms.Label();
+            this.txtTenLop = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.datagrdviewKhoa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // datagrdviewKhoa
+            // 
+            this.datagrdviewKhoa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagrdviewKhoa.Location = new System.Drawing.Point(12, 23);
+            this.datagrdviewKhoa.Name = "datagrdviewKhoa";
+            this.datagrdviewKhoa.RowHeadersWidth = 51;
+            this.datagrdviewKhoa.RowTemplate.Height = 24;
+            this.datagrdviewKhoa.Size = new System.Drawing.Size(555, 473);
+            this.datagrdviewKhoa.TabIndex = 0;
+            // 
+            // txtMaLop
+            // 
+            this.txtMaLop.Location = new System.Drawing.Point(716, 205);
+            this.txtMaLop.Name = "txtMaLop";
+            this.txtMaLop.Size = new System.Drawing.Size(177, 22);
+            this.txtMaLop.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(711, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Chọn khoa";
+            // 
+            // cmbKhoa
+            // 
+            this.cmbKhoa.FormattingEnabled = true;
+            this.cmbKhoa.Location = new System.Drawing.Point(716, 117);
+            this.cmbKhoa.Name = "cmbKhoa";
+            this.cmbKhoa.Size = new System.Drawing.Size(163, 24);
+            this.cmbKhoa.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(711, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mã lớp";
+            // 
+            // lblTenLop
+            // 
+            this.lblTenLop.AutoSize = true;
+            this.lblTenLop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenLop.Location = new System.Drawing.Point(711, 255);
+            this.lblTenLop.Name = "lblTenLop";
+            this.lblTenLop.Size = new System.Drawing.Size(78, 25);
+            this.lblTenLop.TabIndex = 6;
+            this.lblTenLop.Text = "Tên lớp";
+            // 
+            // txtTenLop
+            // 
+            this.txtTenLop.Location = new System.Drawing.Point(716, 296);
+            this.txtTenLop.Name = "txtTenLop";
+            this.txtTenLop.Size = new System.Drawing.Size(177, 22);
+            this.txtTenLop.TabIndex = 5;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(583, 379);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(110, 38);
+            this.btnThem.TabIndex = 7;
+            this.btnThem.Text = "THÊM";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(839, 379);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(110, 38);
+            this.btnSua.TabIndex = 8;
+            this.btnSua.Text = "SỬA";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(709, 379);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(110, 38);
+            this.btnXoa.TabIndex = 9;
+            this.btnXoa.Text = "XÓA";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // frmDaoThanhHung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 562);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.lblTenLop);
+            this.Controls.Add(this.txtTenLop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbKhoa);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMaLop);
+            this.Controls.Add(this.datagrdviewKhoa);
+            this.Name = "frmDaoThanhHung";
+            this.Text = "Quản Lí SInh viên";
+            this.Load += new System.EventHandler(this.frmDaoThanhHung_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.datagrdviewKhoa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView datagrdviewKhoa;
+        private System.Windows.Forms.TextBox txtMaLop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbKhoa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTenLop;
+        private System.Windows.Forms.TextBox txtTenLop;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+    }
+}
+
---
+++ KetNoiDBS/frmDaoThanhHung.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace KetNoiDBS
+{
+    public partial class frmDaoThanhHung : Form
+    {
+        DataSet dset = new DataSet();
+        SqlConnection conn;
+        
+       
+
+        public frmDaoThanhHung()
+        {
+            InitializeComponent();
+            conn = getConnection();
+        }
+
+        
+        private SqlConnection getConnection()
+        {
+            string strConn = "Data Source=LAPTOP-60391V64\\SQLEXPRESS;Initial Catalog=QuanLi_Sinhvien;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(strConn);
+            conn.Open();
+            return conn;
+        }
+
+        private void frmDaoThanhHung_Load(object sender, EventArgs e)
+        {
+
+            this.loadCmbKhoa();
+            this.loadLop();
+            this.binding();
+        }
+
+        private void loadCmbKhoa()
+        {
+            string query = "select * from Khoa";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+            adapter.Fill(dset, "Khoa");
+
+            cmbKhoa.DataSource = dset.Tables["Khoa"];
+            cmbKhoa.DisplayMember = "TenKhoa";
+            cmbKhoa.ValueMember = "MaKhoa";
+        }
+
+        private void loadLop()
+        {
+
+            if (dset.Tables.Contains("Lop"))
+                datagrdviewKhoa.DataSource = dset.Tables["Lop"];
+            else
+            {
+                string query = "select * from Lop";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                adapter.Fill(dset, "Lop");
+                datagrdviewKhoa.DataSource = dset.Tables["Lop"];
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            string maKhoa = cmbKhoa.SelectedValue.ToString();
+            string maLop = txtMaLop.Text;
+            string tenLop = txtTenLop.Text;
+
+            string query = "select * from Lop";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+
+            if (!dset.Tables.Contains("Lop"))
+            {
+                adapter.Fill(dset, "Lop");
+                
+
+            }
+
+            DataRow newRow = dset.Tables["Lop"].NewRow();
+            newRow["MaLop"] = maLop;
+            newRow["TenLop"] = tenLop;
+            newRow["MaKhoa"] = maKhoa;
+
+            dset.Tables["Lop"].Rows.Add(newRow);
+
+            SqlCommandBuilder cmdBuilder = new SqlCommandBuilder(adapter);
+            adapter.Update(dset, "Lop");
+
+            this.loadLop();
+
+            txtMaLop.Text = "";
+            txtTenLop.Text = "";
+
+            txtMaLop.Focus();
+
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+           
+            string query = "select * from Lop";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+            string id = datagrdviewKhoa.SelectedCells[0].OwningRow.Cells[0].Value.ToString();
+
+            DataColumn[] keyColumns = new DataColumn[1];
+            keyColumns[0] = dset.Tables["Lop"].Columns["MaLop"];
+
+            dset.Tables["Lop"].PrimaryKey = keyColumns;
+            DataRow dr = dset.Tables["Lop"].Rows.Find(id);
+
+
+            if (dr != null)
+            {
+                dr.Delete();
+            }
+
+            SqlCommandBuilder cmdBuilder = new SqlCommandBuilder(adapter);
+            adapter.Update(dset, "Lop");
+
+            this.loadLop();
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+
+            string query = "select * from Lop";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+         
+            string id = datagrdviewKhoa.SelectedCells[0].OwningRow.Cells[0].Value.ToString();
+
+            DataColumn[] keyColumns = new DataColumn[1];
+            keyColumns[0] = dset.Tables["Lop"].Columns["MaLop"];
+     
+
+            dset.Tables["Lop"].PrimaryKey = keyColumns;
+            DataRow dr = dset.Tables["Lop"].Rows.Find(id);
+            
+            if (dr != null)
+            {
+                dr["TenLop"] = txtTenLop.Text;
+                dr["MaKhoa"] = cmbKhoa.SelectedValue;
+            }
+            
+
+            SqlCommandBuilder cmdBuilder = new SqlCommandBuilder(adapter);
+            adapter.Update(dset, "Lop");
+            this.loadLop();
+
+        }
+
+        private void binding()
+        {
+           
+            txtMaLop.DataBindings.Clear();
+            txtMaLop.DataBindings.Add("Text", datagrdviewKhoa.DataSource, "MaLop");
+            txtTenLop.DataBindings.Clear();
+            txtTenLop.DataBindings.Add("Text", datagrdviewKhoa.DataSource, "TenLop");
+            cmbKhoa.DataBindings.Clear();
+            cmbKhoa.DataBindings.Add("DisplayMember", datagrdviewKhoa.DataSource, "MaKhoa");
+        }
+    }
+}
